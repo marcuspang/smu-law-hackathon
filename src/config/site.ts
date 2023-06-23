a54ba82025e750bf1dcee86c4949fce81b8d5d91
Marcus Pang <marcuspangyuyang@gmail.com>
2023-06-23 13:11:28 +0800

    feat: setup shadcn ui

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "CaseSummarise",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [],
+  links: {
+    github: "https://github.com/marcuspang",
+  },
+};
